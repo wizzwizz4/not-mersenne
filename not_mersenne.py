a3acb776aeecd12d1f5d554515c072ab0ef778b4
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-01-28 17:17:51 +0000

    Since b is always 0 (the sequence always starts at 1), we can rule out some possibilities.

--- not_mersenne.py
+++ not_mersenne.py
@@ -1,4 +1,4 @@
-def not_mersenne(base) -> (int, int):
+def not_mersenne(base) -> int:
     """Find a repeating pattern in the units digit of the powers of two.
     If that pattern contains 1, then we can rule out an entire category of
     n values from the possibility of (2**n)-1 being prime.
@@ -6,28 +6,16 @@ def not_mersenne(base) -> (int, int):
     Example for base 5: 1, 2, 4, 3, 1, 2, 4, 3, 1, 2, 4, 3
                         therefore (2**(4n+0))-1 is never a Mersenne prime.
 
-    Return a, b where am+b is the class of values of n being ruled out.
+    Return a where am is the class of values of n being ruled out.
     """
 
-    next_digit = {}
+    seen = set()
     digit = 1
-    b = 0
-    while digit not in next_digit:
-        # next_digit[digit] assigned first.
-        next_digit[digit] = digit = (digit * 2) % base
-        b += 1
-    loop = next_digit[digit]
-    a = 1
-    seen_one = digit == 1 or loop == 1
-    while loop != digit:
-        a += 1
-        loop = next_digit[loop]
-        seen_one = seen_one or loop == 1
-    b -= a
-    if seen_one:
-        ## if b == 0: print(next_digit, file=__import__("sys").stderr)
-        return a, b
-    return None, None
+    while digit not in seen:
+        seen.add(digit)
+        digit = (digit * 2) % base
+    if digit == 1:
+        return len(seen)
 
 def primes(known=[2,3,5,7,11]):
     yield from known
@@ -43,8 +31,5 @@ def primes(known=[2,3,5,7,11]):
 
 if __name__ == "__main__":
     for base in primes():
-        a, b = not_mersenne(base)
-        if a is not None:
-            print(a, b, sep="n+")
-        else:
-            print("no", base)
+        a = not_mersenne(base)
+        print(a, end="n\n")
